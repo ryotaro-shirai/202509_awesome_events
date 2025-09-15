ac300425bc600a1c86dc0fbace80e51409909672
ryotaro-shirai <ryotaro.shirai@giftee.co>
2025-09-16 06:47:03 +0900

    Added Log out

--- awesome_events/app/controllers/application_controller.rb
+++ awesome_events/app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  helper_method :logged_in?
+
+  private
+
+  def logged_in?
+    !!session[:user_id]
+  end
 end
