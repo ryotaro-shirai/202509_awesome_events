29695a605188dfe902869f94d8717b12e5db3ed8
ryotaro-shirai <ryotaro.shirai@giftee.co>
2025-09-25 18:52:19 +0900

    Added events edit page

--- awesome_events/app/models/event.rb
+++ awesome_events/app/models/event.rb
@@ -10,6 +10,11 @@ class Event < ApplicationRecord
 
   scope :available,-> { where("start_at > ?", Time.zone.now) }
 
+  def created_by?(user)
+    return false unless user
+    owner_id == user.id
+  end
+
   private
 
   def start_at_should_be_before_end_at
