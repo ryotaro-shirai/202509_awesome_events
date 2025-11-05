c2423b6f3be6a988de1c5ed2e2370aa76f3f4d84
ryotaro-shirai <ryotaro.shirai@giftee.co>
2025-11-06 06:35:30 +0900

    Test: Add tests for time manipulation and display

--- awesome_events/spec/rails_helper.rb
+++ awesome_events/spec/rails_helper.rb
@@ -86,6 +86,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
+  config.include ActiveSupport::Testing::TimeHelpers
 
   config.include SignInHelper
 
--- awesome_events/spec/system/welcome_spec.rb
+++ awesome_events/spec/system/welcome_spec.rb
@@ -7,6 +7,17 @@ RSpec.describe "Welcome", type: :system do
         visit root_path
         expect(page).to have_selector "h1", text: "イベント一覧"
       end
+
+      it "未来のイベントは表示され、過去のイベントは非表示であること" do
+        future_event = FactoryBot.create(:event, start_at: Time.zone.now + 3.day)
+        past_event = FactoryBot.create(:event, start_at: Time.zone.now + 1.day)
+
+        travel_to(Time.zone.now + 2.day) do
+          visit root_path
+          expect(page).to have_selector "h5", text: future_event.name
+          expect(page).not_to have_selector "h5", text: past_event.name
+        end
+      end
     end
   end
 end
